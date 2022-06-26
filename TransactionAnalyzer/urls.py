82d5cfb60d91aa6bdc076ade965c2e3d460ad5c6
Kalimara_Peleteiro <kaliamrapeleteiro@gmail.com>
2022-06-26 14:02:25 -0300

    Adicionando Paginas de Login e Signup

--- TransactionAnalyzer/urls.py
+++ TransactionAnalyzer/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('upload', views.upload, name='upload'),
     path('newfile', views.newfile, name='newfile'),
     path('history', views.history, name='history'),
-    path('profile', views.profile, name='profile')
+    path('profile', views.profile, name='profile'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
 ]
